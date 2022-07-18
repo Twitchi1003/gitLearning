6b462e965745badfb99fb021a0358b3102908e1c
Twitchi <steve1003@hotmail.co.uk>
2022-07-18 12:53:16 +0100

    The first files in a new project

---
+++ Sims/Grav1.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  5 23:57:38 2022
+
+@author: Twitchi
+
+Simulation first steps
+"""
+
+'''Define everything'''
+
+
+class Wanderer:
+    def __init__(self, name, mass, velocity, posX, posY):
+        self.name = name
+        self.mass = mass
+        self.velocity = velocity
+        self.posX = posX
+        self.posY = posY
+    def forces()
+        '''loop all wanderers that are not self'''        
+
+
+'''Welcome user, how many objects? 
+
+store user input:Name, mass poss and speed
+Create wanderer object with user info'''
+
+
